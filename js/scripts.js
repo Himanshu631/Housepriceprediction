da1b752aa07af1a0f94ae3e19ae2e642e7dfa259
Himanshu <55944613+Himanshu631@users.noreply.github.com>
2022-09-20 16:11:30 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,104 @@
+/*!
+* Start Bootstrap - Agency v7.0.11 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+function onClickedEstimatePrice() {
+    console.log("Estimate price button clicked");
+    var sqft = document.getElementById("uiSqft");
+    var bhk = document.getElementById("uibhk");
+    var bathrooms = document.getElementById("uibathrooms");
+    var location = document.getElementById("uiLocations");
+    var estPrice = document.getElementById("uiEstimatedPrice");
+  
+    var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
+    //var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+  
+    $.post(url, {
+        total_sqft: parseFloat(sqft.value),
+        bhk: parseFloat(bhk.value),
+        bath: parseFloat(bathrooms.value),
+        location: location.value
+    },function(data, status) {
+        console.log(data.estimated_price);
+        estPrice.innerHTML = "<h2>The estimated price is " + data.estimated_price.toString() + " Lakh</h2>";
+        console.log(status);
+    });
+  }
+
+function onPageLoad() {
+    console.log( "document loaded" );
+    var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
+    //var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+    $.get(url,function(data, status) {
+        console.log("got response for get_location_names request");
+        if(data) {
+            var locations = data.locations;
+            var uiLocations = document.getElementById("uiLocations");
+            $('#uiLocations').empty();
+            for(var i in locations) {
+                var opt = new Option(locations[i]);
+                $('#uiLocations').append(opt);
+            }
+        }
+    });
+  }
+
+  function myFormResetFunction() {
+    document.getElementById("formreseter").value = "";
+  }
+  
+  window.onload = onPageLoad;
+
+
+
